d03610e444644fa8740558da51104f9d30593ab1
Michael Krhutek <mkrhutek001@cee.ema.ad.pwcinternal.com>
2019-03-05 15:29:56 +0100

    First version

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <HelloWorld/>
   </div>
 </template>
 
@@ -17,12 +17,122 @@ export default {
 </script>
 
 <style>
+/* Light mode */
+/* :root {
+  --primary-color:  rgb(97, 0, 189);
+  --main-grey: rgb(185, 185, 185);
+  --box-shadow-color: rgba(0,0,0,0.6);
+  --main-text-color: rgb(20, 20, 20);
+  --main-background-color: rgb(250, 250, 250);
+   --animation-duration: 0.2s;
+} */
+
+/* Dark mode */
+:root {
+  --primary-color:  rgb(43, 255, 0);
+  --main-grey: rgb(70, 70, 70);
+  --box-shadow-color: rgba(255, 255, 255, 0.6);
+  --main-text-color: rgb(240, 240, 240);
+  --main-background-color: rgb(20, 20, 20);
+  --animation-duration: 0.4s;
+}
+
+html {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+
+body {
+  width: 100%;
+  min-height: 100%;
+  margin: 0;
+  background-color: var(--main-background-color);
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Raleway', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: var(--main-text-color);
+  background-color: var(--main-background-color);
+}
+
+a {
+  color: var(--primary-color);
+}
+
+a:hover {
+  font-weight: bold;
+}
+
+button {
+  font-family: 'Raleway', sans-serif;
+  font-weight: bold;
+  padding: 0.5em 1.0em;
+  text-align: center;
+  display: inline-block;
+  font-size: 1em;
+  border: 1px solid;
+  transition-duration: var(--animation-duration);
+}
+
+.button-primary {
+  background-color: transparent;
+  color: var(--primary-color);
+  border-color: var(--primary-color);
+}
+
+.button-primary:hover {
+  background-color: var(--primary-color);
+  color: var(--main-background-color);
+  border-color: transparent;
+  cursor: pointer;
+}
+
+.slider {
+  -webkit-appearance: none;
+  height: 0.4em;
+  background: var(--main-grey);
+  outline: none;
+  opacity: 0.7;
+  -webkit-transition: var(--animation-duration);
+  transition: opacity var(--animation-duration);
+}
+
+.active-slider:hover {
+  opacity: 1.0;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  width: 1.2em;
+  height: 1.2em;
+  border-radius: 50%;
+}
+
+.active-slider::-webkit-slider-thumb {
+  background: var(--primary-color);
+  cursor: pointer;
+  animation-name: slider-from-disabled-to-active;
+  animation-duration: var(--animation-duration);
+}
+
+.disabled-slider::-webkit-slider-thumb {
+  background: var(--main-grey);
+  cursor: not-allowed;
+  animation-name: slider-from-active-to-disabled;
+  animation-duration: var(--animation-duration);
+}
+
+@keyframes slider-from-active-to-disabled {
+  from {background-color: var(--main-grey);}
+  to {background-color: var(--primary-color);}
+}
+
+@keyframes slider-from-disabled-to-active {
+  from {background-color: var(--main-grey);}
+  to {background-color: var(--primary-color);}
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,96 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="container">
+      <span class="title">Conway's Game of Life</span>
+    </div>
+    <div class="container">
+      <div class="column-container">
+        <div>
+          <span>Size</span>
+        </div>
+        <div>
+          <span class="value">{{ size }} x {{ size }}</span> 
+        </div>
+        <div>
+          <input :class="isTimerRunning ? 'slider disabled-slider' : 'slider active-slider'"
+            type="range" min="4" max="16"
+            v-model="size" @change="createBoard" :disabled="isTimerRunning"
+          > 
+        </div>                          
+      </div>
+      <div class="column-container">
+        <div>
+          <span>Live Probability</span>
+        </div>
+        <div>
+          <span class="value">{{ Math.round(liveProbability * 100) }} %</span>
+        </div>
+        <div>
+          <input :class="isTimerRunning ? 'slider disabled-slider' : 'slider active-slider'"
+            type="range" min="0" max="1" step="0.01"
+            v-model="liveProbability" @change="createBoard" :disabled="isTimerRunning"           
+          >  
+        </div>           
+      </div>
+      <div class="column-container">
+        <div>
+          <span>Speed</span>
+        </div>
+        <div>
+          <span class="value">{{ timerSpeed }} sec</span> 
+        </div>
+        <div>
+          <input :class="isTimerRunning ? 'slider disabled-slider' : 'slider active-slider'"
+            type="range" min="0.4" max="2" step="0.1"
+            v-model="timerSpeed" :disabled="isTimerRunning"
+          > 
+        </div>                
+      </div>
+    </div>
+    <div class="container">
+      <div v-if="!isTimerRunning">
+        <button class="button button-primary" @click="createBoard">Generate</button>
+      </div>
+      <div v-if="!isTimerRunning">
+        <button class="button button-primary" @click="clearBoard">Clear</button>
+      </div>
+      <div v-if="!isTimerRunning">
+        <button class="button button-primary" @click="createNextGeneration">Next</button>
+      </div>
+      <div v-if="!isTimerRunning">
+        <button class="button button-primary" @click="startTimer">Start</button>
+      </div>
+      <div v-if="isTimerRunning">
+        <button class="button button-primary" @click="stopTimer">Stop</button>
+      </div>    
+    </div>
+    <div class="container">
+      <span>Generation: <strong>{{ generationCounter }}</strong></span>
+    </div>
+    <div class="container">
+      <div class="board" v-bind:style="`
+        grid-template-columns: ${gridTemplateStyle};
+        grid-template-rows: ${gridTemplateStyle};
+      `">
+        <div
+          v-for="(cell, index) in currentGeneration"
+          :key="index" :class="cell.isAlive ? 'cell alive' : 'cell dead'"
+          v-bind:style="`
+            grid-column-start: ${cell.column + 1}; 
+            grid-column-end: ${cell.column + 1};
+            grid-row-start: ${cell.row + 1};
+            grid-row-end: ${cell.row + 1};
+            animation-duration: ${timerSpeed / 3}s;
+            ${isEditable? 'cursor: pointer;' : null}
+          `"
+          @click="isEditable ? changeCellLiveState(cell.row, cell.column) : null"  
+        ></div>
+      </div>      
+    </div>
+    <br>
+    <div class="container page-ending">
+      <span>More about <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">Conway's Game of Life</a></span>
+    </div>
   </div>
 </template>
 
@@ -35,24 +99,229 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      size: 8,
+      liveProbability: 0.5,
+      timerSpeed: 1,
+      currentGeneration: [],
+      timer: null,
+      isTimerRunning: false,
+      generationCounter: 0,
+      isEditable: true
+    };
+  },
+  computed: {
+    gridTemplateStyle() {
+      return `repeat(${Math.sqrt(this.currentGeneration.length)}, 1.0em)`
+    }
+  },
+  methods: {
+    createBoard() {
+      const cells = [];
+      for (let r = 0; r < this.size; r++) {
+       for (let c = 0; c < this.size; c++) {
+         const isAlive = Math.random() <= this.liveProbability ? true : false
+         const cell = { row: r, column: c, isAlive: isAlive, liveNeighbours: null }
+         cells.push(cell)
+       } 
+      }
+      this.currentGeneration = cells
+      this.generationCounter = 0
+    },
+    clearBoard() {
+      this.currentGeneration.forEach(cell => {
+        cell.isAlive = false
+      })
+      this.generationCounter = 0
+    },
+    changeCellLiveState(rowIndex, columnIndex) {
+      const certainCell = this.findCell(rowIndex, columnIndex)
+      const isAlive = certainCell.isAlive
+      certainCell.isAlive = !isAlive
+    },
+    findCell(rowIndex, columnIndex) {
+      const certainCell = this.currentGeneration.find(cell => {
+        return cell.row == rowIndex && cell.column == columnIndex
+      })
+      return certainCell
+    }, 
+    calculateLiveNeigboursToOneCell(rowIndex, columnIndex) {
+      let number = 0
+      // Top middle
+      if (columnIndex > 0) {
+        const isCellAlive = this.findCell(rowIndex, columnIndex - 1).isAlive
+        if (isCellAlive) {
+          number++
+        }
+      }    
+      // Top right
+      if (rowIndex < this.size - 1 && columnIndex > 0) {
+        const isCellAlive = this.findCell(rowIndex + 1, columnIndex - 1).isAlive
+        if (isCellAlive) {
+          number++
+        }
+      }
+      // Center right
+      if (rowIndex < this.size - 1) {
+        const isCellAlive = this.findCell(rowIndex + 1, columnIndex).isAlive
+        if (isCellAlive) {
+          number++
+        }
+      }
+      // Bottom right
+      if (rowIndex < this.size - 1 && columnIndex < this.size - 1) {
+        const isCellAlive = this.findCell(rowIndex + 1, columnIndex + 1).isAlive
+        if (isCellAlive) {
+          number++
+        }
+      }
+      // Bottom middle
+      if (columnIndex < this.size - 1) {
+        const isCellAlive = this.findCell(rowIndex, columnIndex + 1).isAlive
+        if (isCellAlive) {
+          number++
+        }
+      }
+      // Bottom left
+      if (rowIndex > 0 && columnIndex < this.size - 1) {
+        const isCellAlive = this.findCell(rowIndex - 1, columnIndex + 1).isAlive
+        if (isCellAlive) {
+          number++
+        }
+      }
+      // Center left
+      if (rowIndex > 0) {
+        const isCellAlive = this.findCell(rowIndex - 1, columnIndex).isAlive
+        if (isCellAlive) {
+          number++
+        }
+      }
+      // Top left
+      if (rowIndex > 0 && columnIndex > 0) {
+        const isCellAlive = this.findCell(rowIndex - 1, columnIndex - 1).isAlive
+        if (isCellAlive) {
+          number++
+        }
+      }     
+      return number      
+    },
+    calculateLiveNeighboursToAllCells() {
+      this.currentGeneration.forEach(cell => {
+        cell.liveNeighbours = this.calculateLiveNeigboursToOneCell(cell.row, cell.column)
+      })
+    },
+    calculateNextLiveState(currentLiveState, liveNeighbours) {
+      if (currentLiveState) {
+        if (liveNeighbours == 2 || liveNeighbours == 3) {
+          return true
+        } else {
+          return false
+        }
+      } else {
+        if (liveNeighbours == 3) {
+          return true
+        } else {
+          return false
+        }
+      }
+    },
+    createNextGeneration() {
+      this.calculateLiveNeighboursToAllCells()
+      const nextGeneration = []
+      let cellsAlive = 0
+      this.currentGeneration.forEach(currentCell => {
+        const isAlive = this.calculateNextLiveState(currentCell.isAlive, currentCell.liveNeighbours)
+        const nextCell = { row: currentCell.row, column: currentCell.column, isAlive: isAlive, liveNeighbours: null }
+        nextGeneration.push(nextCell)
+        if (isAlive) {
+          cellsAlive++
+        }
+      })
+      this.currentGeneration = nextGeneration
+      this.generetionCounter = this.generationCounter++
+      if (cellsAlive == 0) {
+        this.stopTimer()
+      }
+    },
+    startTimer() {
+      this.isTimerRunning = true
+      this.isEditable = false
+      this.timer = setInterval(() => {
+        this.createNextGeneration()
+      }, this.timerSpeed * 1000)
+    },
+    stopTimer() {
+      this.isTimerRunning = false
+      this.isEditable = true
+      clearInterval(this.timer)
+    }
+  },
+  created() {
+    this.createBoard()
   }
+
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .container {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    padding: 1.2em 0 1.2em 0;
+  }
+
+  .container > div {
+    margin: 0 0.2em 0 0.2em;
+  } 
+
+  .column-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;    
+  }
+  
+  .column-container > div {
+    margin: 0.2em 0 0.2em 0;
+  }
+
+  .board {
+    display: grid;
+    grid-row-gap: 0.7em;
+    grid-column-gap: 0.7em
+
+  }
+
+  .title {
+    font-size: 2em;
+    font-weight: bold;
+    font-style: italic;
+  }
+
+  .value {
+    font-size: 2em;
+  }
+
+  .row {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .cell {
+    width: 1.2em;
+    height: 1.2em;
+    box-shadow: 0em 0.25em 0.75em 0px var(--box-shadow-color);
+  }
+
+  .alive {
+    background-color: var(--primary-color);
+    animation-name: from-dead-to-alive;
+  }
+
+  .dead {
+    background-color: var(--main-grey);
+    animation-name: from-alive-to-dead;
+  }
 </style>
